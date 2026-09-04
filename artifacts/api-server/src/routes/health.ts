a131cbfaeab434b5cacc8d9753e6e3ec49696eeb
theintensivist <103211714+theintensivist@users.noreply.github.com>
2026-09-04 10:03:04 +0000

    llll

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -4,7 +4,7 @@ import { HealthCheckResponse } from "@workspace/api-zod";
 const router = Router();
 
 router.get("/healthz", (_req, res) => {
- res.json ({status: "ok" }) as typeof HealthCheckResponse
+ res.json ({status: "ok" })
 });
 
 export default router;

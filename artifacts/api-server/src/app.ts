079bde8a68b232bfc312d96f832b4486836a13b9
theintensivist <103211714+theintensivist@users.noreply.github.com>
2026-09-04 09:35:06 +0000

    fixing app logger

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -6,25 +6,7 @@ import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+app.use (logger)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
